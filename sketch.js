fe3cc0f2d11ebd9e7e39c9b3200ce134dfd34bc0
OlliUlku <olli.ulkun@gmail.com>
2023-06-05 13:26:22 +0300

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
+let jpegPhoto, CNVS;
+
 function setup() {
   // CENTERS CANVAS
-  let CNVS = createCanvas(400, 400);
-  let newWindowX = (windowWidth - width) / 2;
-  let newWindowY = (windowHeight - height) / 2;
-  CNVS.position(newWindowX, newWindowY);
+  CNVS = createCanvas(300, 128);
+  // let newWindowX = (windowWidth - width) / 2;
+  // let newWindowY = (windowHeight - height) / 2;
+  // CNVS.position(newWindowX, newWindowY);
+  pixelDensity(1)
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(0);
+  noStroke();
+  circle(50, 50, 30);
+  circle(150, 50, 30);
+  let canvas = document.getElementById("defaultCanvas0");
+  jpegPhoto = canvas.toDataURL('image/jpeg');
+  print(jpegPhoto)
+
+  createImg(jpegPhoto, 'photo of the canvas')
 }
