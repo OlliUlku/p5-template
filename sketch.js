53c8c145debc16eedbfe220bd01c3208017be78b
OlliUlku <olli.ulkun@gmail.com>
2023-06-12 00:24:01 +0300

    Still testing

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let ZERO = 20;
+let ZERO = 25;
 let ONE = 220;
 let angleX = 0;
 let angleY = 0;
@@ -20,23 +20,20 @@ function setup() {
 function draw() {
   orbitControl();
 
-  background(ZERO);
+  background(0);
 
-  spotLight(ONE,ONE,ONE,0,0,200,0,0,-1,PI / 16,0);
-  spotLight(ONE, ONE, ONE, mouseX - width / 2, mouseY - height / 2, 200, 0, 0, -1, PI / 16, 0);
+  //spotLight(ONE, ONE, ONE, 0, 0, 200, 0, 0, -1, PI / 8, 0);
+  spotLight(10, 10, 10, mouseX - width / 2, mouseY - height / 2, 200, 0, 0, -1, PI / 16, 0);
   //pointLight(ONE, ONE, ONE, 0, 0, 100);
-
-
   noStroke();
-  //stroke('red');
-  //ambientMaterial(ONE,ONE,ONE);
-  fill(ONE);
-  //rotateX(angleX);rotateY(angleY);
-  //rotateY(angleZ);
-  rect(0,0,50);
- // ambientMaterial(ZERO,ZERO,ZERO);
-  //plane(width/2);
-  angleX += 0.1;
-  angleY += 0.09;
-  angleX += 0.02;
+
+  push();
+  emissiveMaterial(ONE, ONE, ONE);
+  plane(width);
+  pop();
+  push()
+  translate(0, 0, 10);
+  emissiveMaterial(ZERO, ZERO, ZERO);
+  plane(20);
+  pop();
 }
