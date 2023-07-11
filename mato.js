fb73b2126bc19fb711bfcae6090cf2ae1a1c761d
OlliUlku <olli.ulkun@gmail.com>
2023-07-11 21:38:50 +0300

    update

--- mato.js
+++ mato.js
@@ -2,26 +2,88 @@ class mato {
     constructor() {
         this.pos = createVector(0, 0, 0);
         this.vel = createVector(0, -1, 0);
-        this.size = 20;
+        this.size = F;
+        this.lastPos = createVector(0, 0, 0);
+        this.lastPos.copy(this.pos);
+        this.instructions = [this.vel.copy()];
+        this.direction = this.vel.copy();
+
+        this.tail = [createVector(0, 1, 0), createVector(0, 1, 0), createVector(0, 1, 0), createVector(0, 1, 0), createVector(0, 1, 0), createVector(0, 1, 0)];
+        print(this.tail);
     }
 
     update() {
+        if (this.instructions.length > 3) {
+            this.instructions.length = 3;
+        }
+
+        this.direction = this.instructions.shift();
+
+        if (this.direction != undefined) {
+            this.vel.set(this.direction);
+        }
+
+        // if (this.vel.x != 1) {
+        //     this.vel.set(-1, 0, 0);
+        // }
+        // if (this.vel.x != -1) {
+        //     this.vel.set(1, 0, 0);
+        // }
+        // if (this.vel.y != 1) {
+        //     this.vel.set(0, -1, 0);
+        // }
+        // if (this.vel.y != -1) {
+        //     this.vel.set(0, 1, 0);
+        // }
+
+
+
+
+        this.lastPos = this.pos.copy();
+
+        this.tail.shift();
+        append(this.tail, this.pos.copy());
+
         this.pos.add(this.vel);
+
+
     }
+
 
     show() {
         push();
-        translate(this.pos.x, this.pos.y, this.pos.z);
+        translate(this.pos.x * F, this.pos.y * F, this.pos.z * F);
         emissiveMaterial(0, 0, 255);
         box(this.size);
         pop();
+        for (let k = 0; k < this.tail.length; k++) {
+            push();
+            let position = this.tail[k].copy();
+            translate(position.x * F, position.y * F, position.z * F);
+            emissiveMaterial(0, 0, 120);
+            box(this.size);
+            pop();
+        }
     }
 
-    TURNLEFT () {
-        this.vel.rotate(-90);
+    TURNLEFT() {
+        append(this.instructions, createVector(-1, 0, 0));
     }
-
-    TURNRIGHT () {
-        this.vel.rotate(90);
+    TURNRIGHT() {
+        append(this.instructions, createVector(1, 0, 0));
+    }
+    TURNUP_Y() {
+        append(this.instructions, createVector(0, -1, 0));
+    }
+    TURNDOWN_Y() {
+        append(this.instructions, createVector(0, 1, 0));
+    }
+    TURNUP_Z() {
+        print('w');
+        append(this.instructions, createVector(0, 0, 1));
+    }
+    TURNDOWN_Z() {
+        print('s');
+        append(this.instructions, createVector(0, 0, -1));
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ let angleY = 0;
 let angleZ = 0;
 let Madot = [];
 let Boxes = [];
+let F = 20;
+let frames = 0;
+let Omput = [];
+
 
 function setup() {
   // CENTERS CANVAS
@@ -12,9 +16,11 @@ function setup() {
   let newWindowX = (windowWidth - width) / 2;
   let newWindowY = (windowHeight - height) / 2;
   CNVS.position(newWindowX, newWindowY);
-  // frameRate(12);
-  rectMode(CENTER);
+  frameRate(60);
+  //rectMode(CENTER);
   angleMode(DEGREES);
+
+
 
 
   for (let i = 0; i < 100; i++) {
@@ -24,11 +30,15 @@ function setup() {
   for (let i = 0; i < 1; i++) {
     Madot[i] = new mato();
   }
+
+  for (let i = 0; i < 5; i++) {
+    Omput[i] = new omena();
+  }
 }
 
 function draw() {
-  // ortho()
-  orbitControl();
+  //ortho();
+  //orbitControl();
 
 
 
@@ -38,27 +48,44 @@ function draw() {
     Boxes[i].light();
   }
 
-  // spotLight(ONE, ONE, ONE, 0, 0, 200, 0, 0, -1, PI / 8, 0);
+  spotLight(ONE, ONE, ONE, 0, 0, 200, 0, 0, -1, PI / 8, 0);
   // pointLight(ONE, ONE, ONE, 0, 0, 100);
   noStroke();
 
   push();
   specularMaterial(ZERO);
-  translate(0, 0, -1);
+  translate(0, 0, -Madot[0].size - 1);
   plane(width);
   pop();
 
+  push();
+  translate(0, 0, -Madot[0].size);
   for (i in Boxes) {
     Boxes[i].update();
     Boxes[i].show();
   }
-  for (i in Madot) {
-    Madot[i].update();
-    Madot[i].show();
+  pop();
+
+  for (i in Omput) {
+    Omput[i].show();
+    // if (frameCount % 22 === 0) {
+    //   Omput[i].update();
+    // }
   }
+
+  for (i in Madot) {
+    Madot[i].show();
+
+    if (frameCount % 30 === 0) {
+      Madot[i].update();
+      frames = 0;
+    }
+  }
+
+  // RotatingBox
   push();
   shininess(10000);
-  specularMaterial(ONE);
+  emissiveMaterial(ONE);
   translate(0, 0, frameCount / 10 + 10);
   rotateX(frameCount / 2);
   rotateY(frameCount / 3);
@@ -73,17 +100,44 @@ function draw() {
   // box(20);
   // pop();
 
-   angleMode(RADIANS)
-   camera(Madot[0].pos.x, Madot[0].pos.y, (height/2) / tan(PI/6), Madot[0].pos.x, Madot[0].pos.y, 0, 0, 1, 0)
-   angleMode(DEGREES);
+  let lerpV = map(frames, 0, 29, 0, 1);
+  let lerpX = lerp(Madot[0].lastPos.x * F, Madot[0].pos.x * F, lerpV);
+  let lerpY = lerp(Madot[0].lastPos.y * F, Madot[0].pos.y * F, lerpV);
+  let camX = lerpX;
+  let camY = lerpY;
+  frames++;
+  if (frames >= 60) {
+    frames = 0;
+  }
+  angleMode(RADIANS);
+
+  camera(camX + 400, camY + 400, (height / 2) / tan(PI / 6), camX, camY, 0, 1, 1, 0);
+  angleMode(DEGREES);
 }
 
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     Madot[0].TURNLEFT();
   }
-  if (keyCode === RIGHT_ARROW) {
+  else if (keyCode === RIGHT_ARROW) {
     Madot[0].TURNRIGHT();
+  }
+  else if (keyCode === UP_ARROW) {
+    Madot[0].TURNUP_Y();
+  }
+  else if (keyCode === DOWN_ARROW) {
+    Madot[0].TURNDOWN_Y();
+  }
+  else if (keyCode === 83) {
+    Madot[0].TURNDOWN_Z();
+  }
+  else if (keyCode === 87) {
+    Madot[0].TURNUP_Z();
+  }
+  else if (keyCode === RETURN) {
+    for (i in Omput) {
+      Omput[i].newPos();
+    }
   }
 }
 
@@ -96,7 +150,6 @@ class Box {
     this.c = color(random(255), random(255), random(255));
     // this.c = color(ONE);
     // this.lightc = color(random(255), random(255), random(255));
-    // print(this.c.toString())
   }
 
   update() {
@@ -117,4 +170,32 @@ class Box {
     pop();
 
   }
+}
+
+class omena {
+  constructor() {
+    this.newPos();
+    print(this.pos);
+    this.size = F;
+  }
+
+
+  newPos() {
+    this.pos = createVector((round(random(-10, 10))), (round(random(-10, 10))), (round(random(0, 20))));
+  }
+
+  update() {
+    this.newPos();
+  }
+
+  show() {
+    push();
+    translate(this.pos.x * F, this.pos.y * F, this.pos.z * F);
+    emissiveMaterial(255, 0, 255);
+    box(this.size);
+    pop();
+  }
+
+
+
 }
